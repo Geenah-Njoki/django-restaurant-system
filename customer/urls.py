523adc7e48bd6b715c94eebeaf33d58d208c2cfe
Geenah-Njoki <georginanjoki7@gmail.com>
2022-01-17 17:19:06 +0300

    Added drop down

--- customer/urls.py
+++ customer/urls.py
@@ -26,6 +26,8 @@ urlpatterns = [
    
    path('password_reset_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password/password-reset-done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password/password-reset-confirm.html"), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password/password-reset-complete.html'), name='password_reset_complete'),      
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password/password-reset-complete.html'), name='password_reset_complete'),    
+      
 ]
+
   
